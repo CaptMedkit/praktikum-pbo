e8443be4577788364c64cafeef463f12a26dbc56
Vicoriza Ramadhani <100180196+CaptMedkit@users.noreply.github.com>
2023-03-12 18:40:06 +0700

    Add files via upload

---
+++ Praktikum-3/bangundatar/Segitiga.java
@@ -0,0 +1,32 @@
+/*	Nama File: Segitiga.java 
+	Pembuat: Vicoriza Ramadhani
+	NIM: 24060121130053
+	LAB: B1
+	08/03/2023
+*/
+
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class Segitiga extends Poligon
+{
+	private double alas, tinggi;
+	
+	public Segitiga(double alas, double tinggi, int jumlahSisi)
+	{
+		this.alas = alas;
+		this.tinggi = tinggi;
+		this.jumlahSisi = jumlahSisi;
+	}
+	
+	public double hitungLuas()
+	{
+		return (alas * tinggi)/2;
+	}
+	
+	public void printInfo()
+	{
+		System.out.println("Bangun Segitiga berisi " + this.getJumlahSisi());
+	}
+}
